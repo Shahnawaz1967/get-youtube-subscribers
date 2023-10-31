06586f103cbb214f48ab2b43cd9e6dd3a7e247b2
Prachi-Rajput <prachirajput210497@gmsil.com>
2023-11-01 00:16:23 +0530

    get api for name and id

--- src/app.js
+++ src/app.js
@@ -28,4 +28,28 @@ app.get("/subscribers", async (req, resp) => {
 
 });
 
+// GET API for getting subscribers by name and Subscribed Channel
+app.get('/subscribers/names', async (req, res) => {
+    try {
+        let subscriberByName = await Subscribers.find().select('name subscribedChannel');
+        res.status(200).send(subscriberByName);
+    } catch (error) {
+        res.status(400);
+    }
+});
+
+
+// GET API for getting subscriber by ID
+app.get('/subscribers/:id', async (req, res) => {
+    try {
+        let subscriberByID = await Subscribers.findById(req.params.id);
+        res.status(200).send(subscriberByID);
+    } catch (error) {
+        res.status(400).send({
+            message: error.message,
+        });
+    }
+});
+
+
 module.exports = app;
